d58f30e0117b3d9c8aa624e425dc9741f3091a4c
Pedro <rpedroricardo87@gmail.com>
2026-04-03 18:44:52 -0300

    chore: config eureka server and client

--- api-ibge/ibgegateway/src/main/java/com/ibge_gateway/ibgegateway/IbgegatewayApplication.java
+++ api-ibge/ibgegateway/src/main/java/com/ibge_gateway/ibgegateway/IbgegatewayApplication.java
@@ -2,8 +2,10 @@ package com.ibge_gateway.ibgegateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class IbgegatewayApplication {
 
 	public static void main(String[] args) {
