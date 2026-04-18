e8802502dce694fe3bb5ad72c81e15370a8c3b06
Pedro <rpedroricardo87@gmail.com>
2026-04-18 17:49:19 -0300

    feat: add localidadesService

--- api-ibge/localidades/src/main/java/com/localidades_services/localidades/localidades/service/LocalidadesService.java
+++ api-ibge/localidades/src/main/java/com/localidades_services/localidades/localidades/service/LocalidadesService.java
@@ -1,4 +1,21 @@
 package com.localidades_services.localidades.localidades.service;
 
 public class LocalidadesService {
+    private final RestTemplate restTemplate;
+
+    @Value("${ibge.api.base-url}")
+    private String baseUrl;
+
+    public List<Object> listarEstados() {
+        String url = baseUrl + "/estados?orderBy=nome";
+        Object[] response = restTemplate.getForObject(url, Object[].class);
+        return Arrays.asList(response);
+    }
+
+    public List<Object> listarMunicipiosPorUf(String uf) {
+        String url = baseUrl + "/estados/" + uf + "/municipios";
+        Object[] response = restTemplate.getForObject(url, Object[].class);
+        return Arrays.asList(response);
+    }
+
 }
