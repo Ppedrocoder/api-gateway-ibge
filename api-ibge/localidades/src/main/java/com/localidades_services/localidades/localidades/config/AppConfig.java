b64230c297d006277ed744390aae97f2ff999cd6
Pedro <rpedroricardo87@gmail.com>
2026-04-18 17:48:50 -0300

    feat: appconfig

--- api-ibge/localidades/src/main/java/com/localidades_services/localidades/localidades/config/AppConfig.java
+++ api-ibge/localidades/src/main/java/com/localidades_services/localidades/localidades/config/AppConfig.java
@@ -1,4 +1,8 @@
 package com.localidades_services.localidades.localidades.config;
 
 public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
