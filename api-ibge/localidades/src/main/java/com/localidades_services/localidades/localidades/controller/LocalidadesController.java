d95352c0626b00acaeead807542b662eedfb1076
Pedro <rpedroricardo87@gmail.com>
2026-04-18 17:49:34 -0300

    feat: add localidadesController

--- api-ibge/localidades/src/main/java/com/localidades_services/localidades/localidades/controller/LocalidadesController.java
+++ api-ibge/localidades/src/main/java/com/localidades_services/localidades/localidades/controller/LocalidadesController.java
@@ -1,4 +1,19 @@
 package com.localidades_services.localidades.localidades.controller;
 
+@RestController
+@RequestMapping("/estados")
+@RequiredArgsConstructor
 public class LocalidadesController {
+
+    private final LocalidadesService service;
+
+    @GetMapping
+    public ResponseEntity<List<Object>> listarEstados() {
+        return ResponseEntity.ok(service.listarEstados());
+    }
+
+    @GetMapping("/{uf}/municipios")
+    public ResponseEntity<List<Object>> listarMunicipios(@PathVariable String uf) {
+        return ResponseEntity.ok(service.listarMunicipiosPorUf(uf));
+    }
 }
